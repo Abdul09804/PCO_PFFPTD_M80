490cdda6f2a22ddce9a580bd02e839f0e94e56b3
bhavana0403 <bhavanabm97@gmail.com>
2024-07-12 08:17:29 +0530

    dictionary

--- 1_Basics/6_dictionary.py
+++ 1_Basics/6_dictionary.py
@@ -6,7 +6,15 @@
             Var = {k1: v1, k2: v2, ..........., kn: vn}
 -> key must be immutable, value can be either mutable or immutable datatype.
 -> The default value of dictionary is {}
-->
+-> To get the value from a dictionary we make use of the syntax,
+            Var[key]
+-> To modify the value, we make use of the syntax
+            Var[key] = value
+            if key is present, it will modify the value. If the key is not present
+            a new key is added
+-> To delete a key value pair we make use of the syntax,
+            del Var[key]
+            if the key is not present, it throws KeyError
 """
 
 d1 = {1: 'one', (6, 5): 1, 'year': 2020}
@@ -36,4 +44,43 @@ print(d4)       # {'one': [1, 2, 3]}
 -> python_visualizer_path = https://pythontutor.com/render.html#mode=edit
 """
 
+d = {'a': 10, 'b': 20, 'a': 40}
+print(d)        # {'a': 40, 'b': 20}
+
+# accessing the values of dictionary
+
+d = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}
+# print(d[1])   # KeyError: 1
+
+print(d['a'])       # 1
+print(d['d'])       # 4
+
+temperatures = dict(Bengaluru=38, Chennai=42, Delhi=40, Kashmir=32)
+print(temperatures)     # {'Bengaluru': 38, 'Chennai': 42, 'Delhi': 40, 'Kashmir': 32}
+print(temperatures['Chennai'])  # 42
+
+print(temperatures['Kashmir'])  # 32
+
+# print(temperatures['Mumbai'])   # KeyError: 'Mumbai'
+
+# modifying the values
+
+temperatures['Chennai'] = 42.6
+print(temperatures)     # {'Bengaluru': 38, 'Chennai': 42.6, 'Delhi': 40, 'Kashmir': 32}
+
+temperatures['Mumbai'] = 43
+print(temperatures)     # {'Bengaluru': 38, 'Chennai': 42.6, 'Delhi': 40, 'Kashmir': 32, 'Mumbai': 43}
+
+# delete the values
+
+del temperatures['Chennai']
+print(temperatures)     # {'Bengaluru': 38, 'Delhi': 40, 'Kashmir': 32, 'Mumbai': 43}
+
+# del temperatures['Patna']       # KeyError: 'Patna'
+
+holidays = {(26, 1): "Republic Day",
+            (15, 8): "Independence Day",
+            (2, 10): "Gandhi Jayanthi"}
+
+print(holidays[(26, 1)])        # Republic Day
 
