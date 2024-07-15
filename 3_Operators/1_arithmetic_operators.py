d809f51945ad5ad5a3bb8d66912b621ed90377e3
bhavana0403 <bhavanabm97@gmail.com>
2024-07-15 08:55:15 +0530

    operators

---
+++ 3_Operators/1_arithmetic_operators.py
@@ -0,0 +1,25 @@
+# Arithemetic Operator
+# 1) Addition Operator
+
+print(6 + True)     # 7
+
+print((7+6j) - 9 - 3.23 + True - False)     # (-4.23+6j)
+
+# print(2 + '2')      # TypeError
+
+print('a' + 'b')        # 'ab' -> concatenation
+
+print('23' + '23')      # 2323
+
+print('23' + 'hai')     # 23hai
+
+print([1, 'hai', 9+7j] + [9, 'hello', 10])      # [1, 'hai', (9+7j), 9, 'hello', 10]
+
+print((1, 'hai', 9+7j) + (9, 'hello', 10))      # (1, 'hai', (9+7j), 9, 'hello', 10)
+
+# print({1, 2, 3} + {4, 5})   # TypeError
+
+# print([1, 'hai', 9+7j] + (9, 'hello', 10))      # TypeError
+
+# print({'a': 10} + {'a': 30})        # TypeError\
+
