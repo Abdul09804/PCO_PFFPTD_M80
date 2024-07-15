a6c81687b87a4e483ea56d58623fbd0f32abc36a
bhavana0403 <bhavanabm97@gmail.com>
2024-07-15 08:34:19 +0530

    type casting

--- 2_Type_Casting/type_casting.py
+++ 2_Type_Casting/type_casting.py
@@ -30,18 +30,18 @@
 
 # 2) Typecasting float to other datatypes
 
-num = 92.23
-
-print(int(num))         # 92
-print(int(9.999999))    # 9
-
-print(complex(num))     # (92.23+0j)
-
-print(bool(num))        # True
-
-print(str(num))         # '92.23'
-
-# print(list(num))        # TypeError:
+# num = 92.23
+#
+# print(int(num))         # 92
+# print(int(9.999999))    # 9
+#
+# print(complex(num))     # (92.23+0j)
+#
+# print(bool(num))        # True
+#
+# print(str(num))         # '92.23'
+#
+# # print(list(num))        # TypeError:
 
 # print(tuple(num))       # TypeError:
 
@@ -53,17 +53,17 @@ print(str(num))         # '92.23'
 
 # 3) Typecasting complex to other datatypes
 
-num = 3-8.3j
-
-# print(int(num))         # TypeError
-
-# print(float(num))       # TypeError
-
-# print(complex(num))     # (3-8.3j)
-
-print(bool(num))        # True
-
-print(str(num))         # '(3-8.3j)'
+# num = 3-8.3j
+#
+# # print(int(num))         # TypeError
+#
+# # print(float(num))       # TypeError
+#
+# # print(complex(num))     # (3-8.3j)
+#
+# print(bool(num))        # True
+#
+# print(str(num))         # '(3-8.3j)'
 
 # print(list(num))        # TypeError
 
@@ -77,22 +77,134 @@ print(str(num))         # '(3-8.3j)'
 
 # 4) Typecasting of boolean to other datatypes
 
-a = True
-b = False
+# a = True
+# b = False
+#
+# print(int(a), int(b))       # 1 0
+#
+# print(float(a), float(b))   # 1.0 0.0
+#
+# print(complex(a), complex(b))   # (1+0j) 0j
+#
+# print(str(a), str(b))       # 'True' 'False'
+#
+# # print(list(a))      # TypeError
+#
+# # print(tuple(a))     # TypeError
+#
+# # print(set(a))       # TypeError
+#
+# # print(dict(a))      # TypeError
 
-print(int(a), int(b))       # 1 0
+########################################################################################
 
-print(float(a), float(b))   # 1.0 0.0
+# 5) Typecasting of string to other datatypes
 
-print(complex(a), complex(b))   # (1+0j) 0j
+a = 'hello'
+b = '12'
 
-print(str(a), str(b))       # 'True' 'False'
+# print(int(a))       # ValueError
+print(int(b))       # 12
+# print(int('12.86'))     # ValueError
 
-# print(list(a))      # TypeError
 
-# print(tuple(a))     # TypeError
+print(float(b))     # 12.0
+print(float('12.85'))   # 12.85
 
-# print(set(a))       # TypeError
+print(complex('8+7j'))    # (8+7j)
+print(complex('3.86'))      # (3.86+0j)
+print(complex('8756'))      # (8756+0j)
 
-# print(dict(a))      # TypeError
+print(bool(a))          # True
+print(bool('12'))       # True
+print(bool('True'))     # True
+print(bool('False'))    # True
+print(bool(''))         # False
+
+print(list(a))          # ['h', 'e', 'l', 'l', 'o']
+
+print(tuple(a))         # ('h', 'e', 'l', 'l', 'o')
+
+print(set(a))           # {'e', 'l', 'o', 'h'}
+
+# print(dict(a))          # ValueError
+
+#######################################################################################
+
+# 6) Typecasting of list to other datatypes
+
+l = [1, 2, 3, 4]
+
+# print(int(l))       # TypeError
+
+# print(float(l))     # TypeError
+
+# print(complex(l))       # TypeError
+
+print(bool(l))          # True
+print(bool([]))         # False
+
+print(str(l))           # '[1, 2, 3, 4]'
+print(len(str(l)))      # 12
+
+print(tuple(l))         # (1, 2, 3, 4)
+
+print(set(l))           # {1, 2, 3, 4}
+
+# print(dict(l))          # TypeError
+
+l = [('a', 1), ('b', 2), ('c', 3)]
+print(dict(l))      # {'a': 1, 'b': 2, 'c': 3}
+
+l = [['a', 1], ['b', 2], ['c', 3]]
+print(dict(l))      # {'a': 1, 'b': 2, 'c': 3}
+
+##########################################################################################
+
+# 7) Typecasting of tuple to other datatypes
+"""
+-> Type casting of tuple is same as typecasting of list to other datatypes
+"""
+
+##########################################################################################
+
+# 8) Typecasting of set to other datatypes
+"""
+-> Type casting of set is same as typecasting of list to other datatypes
+"""
+
+#########################################################################################
+
+# 9) Typecasting of dictionary to other datatypes
+"""
+
+"""
+d = {'a': 1, 'b': 2, 'c': 3}
+
+# print(int(d))       # TypeError
+
+# print(float(d))     # TypeError
+
+# print(complex(d))     # TypeError
+
+print(bool(d))      # True
+print(bool({}))     # False
+
+print(str(d))       # '{'a': 1, 'b': 2, 'c': 3}'
+print(len(str(d)))  # 24
+
+print(list(d))              # ['a', 'b', 'c']
+print(list(d.keys()))       # ['a', 'b', 'c']
+print(list(d.values()))     # [1, 2, 3]
+print(list(d.items()))      # [('a', 1), ('b', 2), ('c', 3)]
+
+print(tuple(d))             # ('a', 'b', 'c')
+print(tuple(d.keys()))      # ('a', 'b', 'c')
+print(tuple(d.values()))    # (1, 2, 3)
+print(tuple(d.items()))     # (('a', 1), ('b', 2), ('c', 3))
+
+print(set(d))               # {'a', 'b', 'c'}
+print(set(d.keys()))        # {'b', 'a', 'c'}
+print(set(d.values()))      # {1, 2, 3}
+print(set(d.items()))       # {('a', 1), ('c', 3), ('b', 2)}
 
