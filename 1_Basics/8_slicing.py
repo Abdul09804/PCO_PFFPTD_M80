1605d363ec3ba1444b6f751a2521f89e7dcae758
bhavana0403 <bhavanabm97@gmail.com>
2024-07-15 07:27:10 +0530

    slicing

--- 1_Basics/8_slicing.py
+++ 1_Basics/8_slicing.py
@@ -57,35 +57,35 @@ h	e	l	l	o		u	n	i	v	e	r	s	e
 # # l nvre
 # print(st[3:13+1:2])
 
-# universe
-print(st[6:14])
-print(st[-8:0])     # ''    # st[6:0]
-print(st[-8:])      # universe
-print(st[6:])       # universe
-
-# olleh
-print(st[-10:-14-1:-1])
-print(st[4:-1:-1])      # st[4:13:-1]   # ''
-print(st[4::-1])        # olleh
-print(st[-10::-1])      # olleh
+# # universe
+# print(st[6:14])
+# print(st[-8:0])     # ''    # st[6:0]
+# print(st[-8:])      # universe
+# print(st[6:])       # universe
+#
+# # olleh
+# print(st[-10:-14-1:-1])
+# print(st[4:-1:-1])      # st[4:13:-1]   # ''
+# print(st[4::-1])        # olleh
+# print(st[-10::-1])      # olleh
 
 """
 -> When we want to fetch the value upto the extreme end(left or right),
    we can ignore writing EI+-1 parameter in slicing syntax
             Var[SI::SV]
 """
-
-# hello
-print(st[0:5])
-print(st[:5])
-print(st[:-9])
-print(st[-14:-9])
-
-# esrevinu
-print(st[13:5:-1])
-print(st[-1:-9:-1])
-print(st[:-9:-1])
-print(st[:5:-1])
+#
+# # hello
+# print(st[0:5])
+# print(st[:5])
+# print(st[:-9])
+# print(st[-14:-9])
+#
+# # esrevinu
+# print(st[13:5:-1])
+# print(st[-1:-9:-1])
+# print(st[:-9:-1])
+# print(st[:5:-1])
 
 """
 -> When we want to fetch the values from the extreme end(right or left)
@@ -95,19 +95,19 @@ print(st[:5:-1])
 
 # reverse the given string
 
-# esrevinu olleh
-print(st[::-1])     # esrevinu olleh
-
-# hello universe
-print(st[::])
-print(st)
-
-
-# first 2 values
-print(st[:2])
-
-# first 5 values
-print(st[:5])
+# # esrevinu olleh
+# print(st[::-1])     # esrevinu olleh
+#
+# # hello universe
+# print(st[::])
+# print(st)
+#
+#
+# # first 2 values
+# print(st[:2])
+#
+# # first 5 values
+# print(st[:5])
 
 """
 -> To fetch the first 'n' values we make use of the syntax,
@@ -115,10 +115,10 @@ print(st[:5])
 """
 
 # last 2 values
-print(st[-2:])
-
-# last 5 values
-print(st[-5:])
+# print(st[-2:])
+#
+# # last 5 values
+# print(st[-5:])
 
 """
 -> To get the last 'n' values we make use of the syntax,
@@ -129,14 +129,29 @@ print(st[-5:])
 
 sam = [4, 9.75, 'hello', ['john', 'steve', 'mary'], {8, 56, 93}, (6, 3)]
 
-# 9.75
-# 'steve'
-# 'mary'
-# 'yram'
-# (3, 6)
-# ['mary', 'steve']
-# 4
-# [4]
+# # 9.75
+# print(sam[1])
+#
+# # 'steve'
+# print(sam[3][1])
+#
+# # 'mary'
+# print(sam[3][2])
+#
+# # 'yram'
+# print(sam[3][2][::-1])
+#
+# # (3, 6)
+# print(sam[5][::-1])
+#
+# # ['mary', 'steve']
+# print(sam[3][2:0:-1])
+#
+# # 4
+# print(sam[0])
+#
+# # [4]
+# print(sam[0:0+1])
 
 pyspiders = {'development': ['python',
                              'sql',
@@ -149,19 +164,36 @@ pyspiders = {'development': ['python',
              }
 
 # python
-# sql
-# html
-# testing
-# ssc
-# clts
-# ognajd
+print(pyspiders['development'][0])
+print(pyspiders['development'][0])
 
+# sql
+print(pyspiders['development'][1])
+print(pyspiders['testing'][1])
+
+# html
+print(pyspiders['development'][2]['web technology'][0])
+
+# testing
+print(pyspiders['testing'][2]['manual testing'][1])
+
+# ssc
+print(pyspiders['development'][2]['web technology'][1][::-1])
+
+# clts
+print(pyspiders['testing'][2]['manual testing'][2][::-1])
+
+# ognajd
+print(pyspiders['development'][3][::-1])
+
+#############################################################################
 
 sam = {'a': ['java', 'python'],
        'b': ('manual', 'automation'),
        'c': {'data', 'sql', 'manual'},
        'd': {'e': {'f': [1, 2, [3, 4]]}}
        }
+
 
 # automation
 # manual
