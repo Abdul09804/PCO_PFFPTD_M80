34da6275c699e725ffd49ae3428546e8c8e354df
bhavana0403 <bhavanabm97@gmail.com>
2024-07-23 07:48:45 +0530

    if statement

---
+++ 5_Control_Statements/1_Conditional_Statements/1_if_statement.py
@@ -0,0 +1,40 @@
+# if statement
+"""
+-> 'if' is a keyword which is used to check the condition
+-> if the condition is True, control will execute TSB
+-> Syntax:
+            if <condition>:
+                TSB
+"""
+
+# 1) WAP to check if a given number is an even number
+# num = int(input('Enter the number : '))     # 18
+# if num % 2 == 0:
+#     print('even number')
+
+# 2) WAP to check if a given number is a odd number
+
+# 3) WAP to check if a given number is a multiple of 7 -> num % 7 == 0
+
+# 4) WAP to check if a number is a multiple of 5 but not divisible by 10 -> num % 10 == 5 -> num % 5 == 0 and num % 10 != 0
+
+# 5) WAP to check whether a given character is an uppercase alphabet
+# char = input('Enter the character : ')
+# if 'A' <= char <= 'Z':
+#     print('Uppercase Character')
+
+# 6) WAP to check whether a given character is a lowercase character
+char = input('Enter the character : ')
+if 'a' <= char <= 'z':
+    print('Lowercase Character')
+
+# 7) WAP to check whether a given character is an alphabet
+""" 'A'<=ch<='Z' or 'a'<=ch<='z' """
+
+# 8) WAP to check whether a given character is a alphanumeric
+# 9) WAP to check if a given character is a special character
+
+
+
+
+
