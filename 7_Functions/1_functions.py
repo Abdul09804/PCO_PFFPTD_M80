b9e2dbe5c28583fb9e68382f10ea3f3872b6aebb
bhavana0403 <bhavanabm97@gmail.com>
2024-08-06 07:22:30 +0530

    functions

---
+++ 7_Functions/1_functions.py
@@ -0,0 +1,33 @@
+# Functions
+"""
+-> Functions are the named memory block which has some set of instructions used to
+   perform a specific task
+   1) Inbuilt Functions - print, input, min, max, etc
+   2) User Defined Functions
+
+User Defined Functions
+-> These are the functions which are created based on user requirements
+-> Syntax:
+            def fname(args):                # function declaration
+                function statement/ SB      # function definition
+                return values
+            fname(args)                     # function call
+            # where passing the arguments and returning the value are not mandatory
+-> def - it is a keyword which is used to create a function
+-> fname - name given to identify some set of instructions
+-> args - parameters/ values which are used to perform the task
+-> return - a keyword which makes he control come out of the function by taking some values
+-> function call - only if we call the function it will be executed
+
+User Defined functions are classified into 4 types
+1) Function without arguments and without return values - reverse
+2) Function with arguments and without return values - append, insert, extend
+3) Function without arguments and with return values - upper, lower, swapcase,
+4) Function with arguments and with return values - count, type, id
+"""
+
+
+
+
+
+
