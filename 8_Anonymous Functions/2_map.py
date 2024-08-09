72bf4240af4dcc39408de06c05d5a18279f00588
bhavana0403 <bhavanabm97@gmail.com>
2024-08-09 07:29:49 +0530

    map

--- 8_Anonymous Functions/2_map.py
+++ 8_Anonymous Functions/2_map.py
@@ -50,5 +50,37 @@ l2 = ['a', 'b', 'c']
 res = map(lambda item1, item2: (item1, item2), l1, l2)
 print(list(res))    # [(1, 'a'), (2, 'b'), (3, 'c')]
 
+##########################################################################################
+
+# WAP to print the list of numbers and its factorial between the range 1 to 10
+
+from math import factorial
+num_fact = map(lambda num: (num, factorial(num)), range(1, 11))
+print(list(num_fact))
+
+# WAP to get the factorial of numbers from 1 to 10
+
+fact = map(factorial, range(1, 11))
+print(list(fact))
+
+# WAP to get the following output
+words = ['hello', 'good', 'morning']
+# o/p1 = [5, 4, 7]
+# o/p2 = ['HELLO', 'GOOD', 'MORNING']
+
+out1 = map(len, words)
+print(list(out1))       # [5, 4, 7]
+
+out2 = map(str.upper, words)
+print(list(out2))       # ['HELLO', 'GOOD', 'MORNING']
+
+nums = [3, -78, -65, 978, -64, 765, -7]
+#o/p = [3, 78, 65, 978, 64, 765, 7]
+out = map(abs, nums)
+print(list(out))        # [3, 78, 65, 978, 64, 765, 7]
+
+
+
+
 
 
