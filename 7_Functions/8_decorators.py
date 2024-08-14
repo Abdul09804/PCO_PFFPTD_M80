7beb3d253915b319b3eb11e91120968fb8e4ddaf
bhavana0403 <bhavanabm97@gmail.com>
2024-08-14 08:20:49 +0530

    decorators

--- 7_Functions/8_decorators.py
+++ 7_Functions/8_decorators.py
@@ -29,25 +29,60 @@
 
 ##################################################################################
 
-def sam(a):
+# def sam(a):
+#     def inner():
+#         print(a)
+#     return inner
+#
+#
+# li = [10, 20, 30]
+# print(li)       # [10, 20, 30]
+#
+# a = 10
+# # print(inner())      # NameError
+#
+# print(sam(12))       # <function sam.<locals>.inner at 0x000001D672DCAF20>
+#
+# sam1 = sam(100)     #
+# sam2 = sam(200)     #
+# print(sam1)     # <function sam.<locals>.inner at 0x000002063128AF20>
+# print(sam2)     # <function sam.<locals>.inner at 0x0000026DA180F560>
+#
+# sam1()      # 100
+# sam2()      # 200
+
+
+def sam(func):
     def inner():
-        print(a)
+        print(func)
+        return func()
+
     return inner
 
+def greeting():
+    return "Hello World"
 
-li = [10, 20, 30]
-print(li)       # [10, 20, 30]
+print(greeting())       # Hello World
 
-a = 10
-# print(inner())      # NameError
+a = sam(greeting)
+print(a)    # <function sam.<locals>.inner at 0x000001CFF837F560>
 
-print(sam(12))       # <function sam.<locals>.inner at 0x000001D672DCAF20>
 
-sam1 = sam(100)     #
-sam2 = sam(200)     #
-print(sam1)     # <function sam.<locals>.inner at 0x000002063128AF20>
-print(sam2)     # <function sam.<locals>.inner at 0x0000026DA180F560>
+def demo():
+    return "In demo"
 
-sam1()      # 100
-sam2()      # 200
+b = sam(demo)
+print(b)        # <function sam.<locals>.inner at 0x0000014C36C5F6A0>
 
+
+print(a())
+"""
+<function greeting at 0x0000017C6AA8AF20>
+Hello World
+"""
+
+print(b())
+"""
+<function demo at 0x000001DEF976F600>
+In demo
+"""
