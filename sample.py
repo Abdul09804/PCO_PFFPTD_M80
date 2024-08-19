e6790a95d9d2efa0de7cb4b34b25b220023e00ea
bhavana0403 <bhavanabm97@gmail.com>
2024-08-19 09:02:46 +0530

    custom sorting

---
+++ sample.py
@@ -0,0 +1,40 @@
+
+d={'a':1,'b':2,'c':3,'d':4}
+d = {'a': 1, 'b': 2, 'c': 3, 'd': 4}
+
+
+d = { 'a' : 1 , 'b' : 2 , 'c' : 3 , 'd' : 4   }
+
+l=[10,23,'hai,hello','98']
+l = [10, 23, 'hai,hello', '98']
+
+word=98
+word= 98
+word = 98
+
+print(4*3)
+print(4+3)
+print(4 * 3)
+print(4 + 3)
+
+a = 4 * 3 + 7 * 2 - 6 * 2 / 3
+a = 4*3 + 7*2 - (6*2)/3
+
+a = 10
+sentence = 'hello everyone good morning'
+li = {'a': 1, 'b': 2, 'c': 3, 'd': 4}
+
+a        = 10
+sentence = 'hello everyone good morning'
+li       = {'a': 1, 'b': 2, 'c': 3, 'd': 4}
+
+st = 'hello world'
+print(st[2 : 5 : 2])
+print(st[2:5:2])
+
+
+li = [1, 2, 3]
+li.sort(reverse = True)
+print(li.sort(reverse=True))
+
+# pep8
