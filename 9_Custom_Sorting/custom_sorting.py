09c1a250a9992694892e9ba422808b598804b0ab
bhavana0403 <bhavanabm97@gmail.com>
2024-08-19 09:00:05 +0530

    custom sorting

--- 9_Custom_Sorting/custom_sorting.py
+++ 9_Custom_Sorting/custom_sorting.py
@@ -71,3 +71,83 @@ print(sorted(names, key=lambda st: st[0]))
 ['alex', 'anthony', 'amar', 'eve', 'john', 'mary', 'steve', 'stella']
 ['alex', 'anthony', 'amar', 'eve', 'john', 'mary', 'steve', 'stella']
 """
+
+##########################################################################################
+
+d = {'hello': 5, 'everyone': 8, 'good': 4, 'evening': 7}
+
+print(d.items())            # [('hello', 5), ('everyone', 8), ('good', 4), ('evening', 7)]
+
+print(sorted(d.items()))    # [('evening', 7), ('everyone', 8), ('good', 4), ('hello', 5)]
+
+# sort based on keys
+print(sorted(d.items(), key=lambda item: item[0]))      # [('evening', 7), ('everyone', 8), ('good', 4), ('hello', 5)]
+
+# sort based on values
+print(sorted(d.items(), key=lambda item: item[-1]))     # [('good', 4), ('hello', 5), ('evening', 7), ('everyone', 8)]
+
+# sort based on last character of the key
+print(sorted(d.items(), key=lambda item: item[0][-1]))      # [('good', 4), ('everyone', 8), ('evening', 7), ('hello', 5)]
+
+#######################################################################################
+
+d = {'hello': 'everyone', 'happy': 'morning', 'well': 'done', 'good': 'handwriting'}
+
+# sort based on length of key
+# sort based on length of value
+# sort based on first character of key
+# sort based on last character of value
+
+#########################################################################################
+
+prices = {
+    'ACME': 45.23,
+    'AAPL': 634.87,
+    'IBM': 254.8,
+    'HPQ': 48.87,
+    'FB': 10.75
+}
+
+
+# sort based on prices
+print(sorted(prices.items(), key=lambda item: item[1]))     # [('FB', 10.75), ('ACME', 45.23), ('HPQ', 48.87), ('IBM', 254.8), ('AAPL', 634.87)]
+
+# sort based on names
+print(sorted(prices.items(), key=lambda item: item[0]))     # [('AAPL', 634.87), ('ACME', 45.23), ('FB', 10.75), ('HPQ', 48.87), ('IBM', 254.8)]
+
+# print the smallest and largest priced share along with share names
+smallest, *rest, largest = sorted(prices.items(), key=lambda item: item[-1])
+print(smallest)     # ('FB', 10.75)
+print(largest)      # ('AAPL', 634.87)
+
+# print the second largest share
+*rest, second_largest, largest = sorted(prices.items(), key=lambda item: item[-1])
+print(second_largest)       # ('IBM', 254.8)
+
+# print the first 2 least priced shares
+# print the first 2 most priced shares
+
+######################################################################################
+
+# get the largest and smallest word in the sentence
+
+st = 'python is a programming language'
+
+words = st.split()
+
+smallest, *rest, largest = sorted(words, key=len)
+print(smallest, largest)    # a programming
+
+####################################################################################
+
+# get the largest and smallest non-repeating word in the sentence
+
+st = 'python is a programming language and programming is easy'
+
+non_repeating_words = [word for word in st.split() if st.split().count(word) == 1]
+print(non_repeating_words)      # ['python', 'a', 'language', 'and', 'easy']
+
+smallest, *rest, largest = sorted(non_repeating_words, key=len)         #
+print(smallest)     # a
+print(largest)      # language
+
