8f94cca3aa8d6c07815aab57afa80cb1a37fcf40
bhavana0403 <bhavanabm97@gmail.com>
2024-08-23 08:30:29 +0530

    oops basics

--- 11_OOPS/2_object_method.py
+++ 11_OOPS/2_object_method.py
@@ -34,8 +34,8 @@ class Point:
         self.y = val
 
 
-p1 = Point(10, 20)
-p2 = Point(30, 40)
+# p1 = Point(10, 20)
+# p2 = Point(30, 40)
 
 # p1.add_value(2)
 # Point.add_value(p2, 0.5)
@@ -46,11 +46,11 @@ p2 = Point(30, 40)
 # p1.swap()
 # Point.swap(p2)
 
-Point.reset(p1)
-p2.reset(4)
-
-print(p1.__dict__)
-print(p2.__dict__)
+# Point.reset(p1)
+# p2.reset(4)
+#
+# print(p1.__dict__)
+# print(p2.__dict__)
 
 ###############################################################################
 
@@ -68,7 +68,52 @@ class Calculator:
     def mul(self, a, b):
         return a * b
 
-c1 = Calculator()
-# print(c1.add(9, 7))
-print(c1.mul('hai', 2))
-# print(c1.mul('hai', 'hai'))     # TypeError
+# c1 = Calculator()
+# # print(c1.add(9, 7))
+# print(c1.mul('hai', 2))
+# # print(c1.mul('hai', 'hai'))     # TypeError
+
+##############################################################################
+
+class Library:
+    books = []
+    def __init__(self, title, category, author):
+        self.title = title
+        self.category = category
+        self.author = author
+        Library.books.append(self)
+
+    def display(self):
+        print('*' * 50)
+        print(f"Title : {self.title}")
+        print(f"Category : {self.category}")
+        print(f"Author : {self.author}")
+        print('*' * 50)
+
+
+b1 = Library("Wings Of Fire", "Autobiography", "Abdul Kalam")
+b2 = Library("One Indian Girl", "Novel", "Chetan Bhagat")
+b3 = Library("A Suitable Boy", "Novel", "Vikram Seth")
+b4 = Library("Ghosts of the Silent Hills", "Horror", "Anita Krishan")
+b5 = Library("The Girl in Room 105", "Love Story", "Chetan Bhagat")
+
+books = Library.books
+for book in books:
+    print(book.title)
+
+
+# print the title of all novels
+for book in books:
+    if book.category == "Novel":
+        print(book.title)
+
+# print the details of all the books
+for book in books:
+    book.display()
+
+# print the details of Chetan Bhagat's books
+for book in books:
+    if book.author == "Chetan Bhagat":
+        book.display()
+
+# create a class of your choice with 3 class members, 3 object members and 3 object methods
