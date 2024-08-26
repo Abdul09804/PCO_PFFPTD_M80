d558b593cfea051648a84a5db001aeba11a30980
bhavana0403 <bhavanabm97@gmail.com>
2024-08-26 08:06:05 +0530

    inheritance

---
+++ 11_OOPS/5_inheritance.py
@@ -0,0 +1,41 @@
+"""
+There are 4 main pillars of object-oriented programming
+1) Inheritance
+2) Polymorphism
+3) Abstraction
+4) Encapsulation
+"""
+"""
+Inheritance
+-> Here, we derive the properties from one class to another class
+-> Inheritance is a mechanism for creating a new class that specifies or modifies 
+   the behavior of an existing class
+-> Class from which we inherit the properties is called parent class/ base class/
+   super class
+-> Class to which we inherit the properties is called subclass/ derived class/ child
+   class/ subtype
+-> Child class will inherit the properties from parent class, it may add new 
+   attributes or redefine any of the properties
+-> There are 5 types
+    1) Single Level Inheritance
+    2) Multi Level Inheritance
+    3) Multiple Inheritance
+    4) Hierarchical Inheritance
+    5) Hybrid Inheritance
+"""
+
+"""
+Single Level Inheritance
+-> Deriving the properties from one class to another class by considering one
+   level is called single level inheritance.
+-> Every class is a kind of single level inheritance because it inherits some of
+   properties (methods) from object class 
+-> Syntax:
+            class PC:
+                SB
+            class CC(PC):
+                SB
+"""
+
+
+
