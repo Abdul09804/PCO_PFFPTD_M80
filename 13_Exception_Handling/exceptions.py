0c11f7dd7eae3c760b6632541b77bf23e7e4a59b
bhavana0403 <bhavanabm97@gmail.com>
2024-09-10 08:07:43 +0530

    exception handling

--- 13_Exception_Handling/exceptions.py
+++ 13_Exception_Handling/exceptions.py
@@ -37,3 +37,67 @@ t = (1, 2)
 # with open('sample1.txt', 'x') as file:      # FileExistsError
 #     pass
 
+"""
+-> We can handle these exceptions using try and except block 
+-> try block includes problem statement which throws an error
+-> except block takes control when try block has some exceptions
+-> 1) Specific Exception Handling
+   2) Generic Exception Handling
+   3) Default Exception Handling
+"""
+
+# 1) Specific Exception Handling
+
+def add():
+    a = eval(input('Enter the value of a : '))
+    b = eval(input('Enter the value of b : '))
+    print(a + b)
+
+# add()
+
+# try:
+#     add()
+# except TypeError:
+#     print("Datatype not matching")
+
+# try:
+#     add()
+# except TypeError as message:
+#     print(message)
+
+
+# def add():
+#     try:
+#         a = eval(input('Enter the value of a : '))
+#         b = eval(input('Enter the value of b : '))
+#         print(a + b)
+#     except TypeError as message:
+#         print(message)
+#         add()
+#
+# add()
+
+"""
+The above program can handle only TypeError but not SyntaxError
+"""
+
+# a = int(input())
+# b = int(input())
+
+
+# def add():
+#     try:
+#         a = int(input('Enter the value of a : '))
+#         b = int(input('Enter the value of b : '))
+#         print(a + b)
+#     except ValueError as msg:
+#         print(msg)
+#         add()
+#
+# add()
+
+
+
+
+
+
